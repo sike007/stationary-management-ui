e9ce6d64c8c826963ee8f9f33b0238f2de8efc72
Santhan770 <santhanrajrapaka@gmail.com>
2023-07-13 10:01:14 +0530

    login page ui

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,9 +1,12 @@
 import { Card } from "@mui/material"
-
+import Button from '@mui/material/Button';
 const Home = () => {
     return (
         <Card className="App-Card">
             <h3>Home</h3>
+        <Button variant="contained" href="./login" style={{float: 'right'}} color="primary" >
+             Login
+        </Button>
         </Card>
     )
 }
--- src/components/PageRoutes.js
+++ src/components/PageRoutes.js
@@ -4,7 +4,9 @@ import Home from "./Home"
 import About from "./About"
 import Contact from "./Contact"
 import NotFound from "./NotFound"
-
+import Login from "./Login"
+import DashboardAdmin from "./DashboardAdmin"
+import DashboardStudent from "./DashboardStudent"
 const PageRoutes = () => {
     return (
         <Routes>
@@ -12,6 +14,9 @@ const PageRoutes = () => {
           <Route exact path="/about" element={<About />} />
           <Route exact path="/contact" element={<Contact />} />
           <Route exact path="/student" element={<Student />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/login1" element={<DashboardAdmin />} />
+          {/* <Route exact path="/login2" element={<DashboardStudent />} /> */}
           <Route path='*' element={<NotFound />}/>
         </Routes>
     )
