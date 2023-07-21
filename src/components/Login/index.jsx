9d9fb7d11332972a1df28ccb31a29f39603cedc3
Santhan770 <santhanrajrapaka@gmail.com>
2023-07-21 13:10:15 +0530

    login page

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -9,6 +9,11 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import axios from "axios";
 import { styled } from '@mui/material/styles';
+import bgimg from "../../images/Login_image2.jpg";
+import Stack from "@mui/material/Stack";
+import Link from '@mui/material/Link';
+import Grid from "@mui/material/Grid";
+import Logo from "../Logo";
 
 const instance = axios.create({
   baseURL: 'http://localhost:8080/api'
@@ -35,7 +40,6 @@ const CssTextField = styled(TextField)({
 });
 
 const Login = ({ setToken }) => {
-
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -60,63 +64,158 @@ const Login = ({ setToken }) => {
 
   };
 
+
+  const boxstyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "75%",
+  height: "70%",
+  bgcolor: "white",
+  boxShadow: 24,
+};
+
+const center = {
+  position: "relative",
+  top: "50%",
+  left: "37%",
+};
+
   return (
-    <Container component="main" maxWidth="xs" >
-      <grid>
-      <grid>
-      <Box
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-         
+    <>
+      <header className="App-header">      
+      <Box sx={{ display: 'flex', alignItems: 'center',marginTop: "0px", marginLeft: "20px"}}>
+          <Box sx={{marginRight:"20px"}}>
+            <Logo />
+          </Box>
+          <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+              Stationery Management System
+          </Typography>
+        </Box>
+      </header>
+      <div
+        style={{
+          backgroundColor: "#880808",
+          backgroundSize: "cover",
+          height: "100vh",
+          color: "#f5f5f5",
         }}
       >
-        <Avatar sx={{ m: 1, bgcolor: '#c11d1d' }}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-         
-          <CssTextField 
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="email"
-            name="email"
-            autoFocus
-          />
-          <CssTextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 , bgcolor: '#c11d1d', ':hover': {
-              bgcolor: '#c11d1d', // theme.palette.primary.main
-              color: 'white',
-            },}}
-          >
-            Sign In
-          </Button>
+        <Box sx={boxstyle}>
+          <Grid container>
+            <Grid item xs={12} sm={12} lg={6}>
+              <Box
+                style={{
+                  backgroundImage: `url(${bgimg})`,
+                  backgroundcolour: "white",
+                  backgroundSize: "cover",
+                  height: "70vh",
+                  color: "#f5f5f5",
+                }}
+              ></Box>
+            </Grid>
+            <Grid item xs={12} sm={12} lg={6}>
+              <Box
+                style={{
+                  backgroundSize: "cover",
+                  height: "70vh",
+                  minHeight: "500px",
+                  backgroundColor: "#FFFFFF",
+                }}
+                sx={{ borderLeft: 2, }}
+              >
 
+                  <Container>
+                    <Box  sx={center} height='75px' />
+                      <Box >
+                        <Box display="flex" alignItems="center" justifyContent="center">
+                          <Avatar
+                            sx={{ ml: "0px", mb: "4px", bgcolor: "#000000" }}
+                          >
+                            <LockOutlinedIcon />
+                          </Avatar>
+                          </Box>
+                        <Box display="flex" alignItems="center" justifyContent="center">
+                        <Typography color="black" component="h1" variant="h4">
+                          Sign In
+                        </Typography>
+                        </Box>
+                    </Box>
+                    <Box
+                      component="form"
+                      noValidate
+                      onSubmit={handleSubmit}
+                      sx={{ mt: 2 }}
+                    >
+                      <Grid container spacing={1}>
+                        <Grid item xs={12} sx={{ ml: "3em", mr: "3em" }}>
+                          <TextField
+                            required
+                            fullWidth
+                            id="email"
+                            label="Username"
+                            name="email"
+                            autoComplete="email"
+                          />
+                        </Grid>
+                        <Grid item xs={12} sx={{ ml: "3em", mr: "3em" }}>
+                          <TextField
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="new-password"
+                          />
+                        </Grid>
+                        <Grid item xs={12} sx={{ ml: "3em", mr: "3em" }}>
+                          <Stack direction="row" spacing={2}>
+
+                            <Typography
+                              variant="body1"
+                              component="span"
+                              onClick={() => {}}
+                              style={{color:"black", marginTop: "10px", cursor: "pointer" }}
+                            >
+                                <Link href="#" variant="body2">
+                                    Forgot password?
+                                </Link>
+                            </Typography>
+                          </Stack>
+                        </Grid>
+                        <Grid item xs={12} sx={{ ml: "5em", mr: "5em" }}>
+                          <Button
+                            type="submit"
+                            variant="contained"
+                            fullWidth="true"
+                            size="large"
+                            sx={{
+                              mt: "10px",
+                              mr: "20px",
+                              borderRadius: 28,
+                              color: "#ffffff",
+                              minWidth: "170px",
+                              backgroundColor: "#880808",
+                            }}
+                          >
+                            Sign in
+                          </Button>
+                        </Grid>
+                      </Grid>
+                    </Box>
+                  </Container>
+              </Box>
+            </Grid>
+          </Grid>
         </Box>
-      </Box></grid>
-      <grid>
-            <item>item2</item>
-      </grid></grid>
-    </Container>
+      </div>
+      <footer className="App-footer">
+        <a >About US</a>
+        <a >Contact US</a>
+      </footer>
+    </>
   );
 }
 
