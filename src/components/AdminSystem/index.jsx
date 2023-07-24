102dc465970bbbf0cc79691254f91e0c84127cf9
itachi <fao@itachi.local>
2023-07-24 18:46:21 +0530

    errors

--- src/components/AdminSystem/index.jsx
+++ src/components/AdminSystem/index.jsx
@@ -160,7 +160,7 @@ const AdminSystem = () => {
                         <th> Item Name </th>
                         <th> Item Quantity </th>
                         <th> returnable type </th>
-                        <th> max days</th>
+                        <th> Borrow days</th>
                         <th> actions</th>
                     </thead>
                     <tbody>
@@ -252,7 +252,7 @@ const AdminSystem = () => {
                 <DialogContent className="pr-4">
                 <Grid container spacing={2} >
                     <Grid container item xs={5} direction="column" >
-                    <>max Days :</>
+                    <>Borrow days :</>
                         
                     </Grid>
                     <Grid direction="column" >
@@ -266,7 +266,7 @@ const AdminSystem = () => {
                <DialogContent className="pr-4">
                <Grid container spacing={2} >
                     <Grid container item xs={5} direction="column" >
-                    <>returnable type : </>
+                    <>returnable type: </>
                     </Grid>
                     <Grid container item xs={4} direction="column" >
                         <test>{ret ? (<test>yes</test>) : (<test>no</test>)}&nbsp;&nbsp;</test>
@@ -369,7 +369,7 @@ const AdminSystem = () => {
                                 <div>
                                 <Grid container spacing={2} >
                                 <Grid container item xs={5} direction="column" >
-                                    <>max days :</>
+                                    <>Borrow days :</>
                                     
                                 </Grid>
                                 <Grid container item xs={4} direction="column" >
